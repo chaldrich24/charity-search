5d054fb05bb56b7d91bfb7c9adf55e424122e064
Snake <chaldrich24@gmail.com>
2021-09-30 14:39:08 -0400

    fetch method add

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var getCharityData = function() {
+    var apiUrl = "https://data.orghunter.com/v1/charitysearch?user_key=087d90e10dc627fdf0b4f716aacb3e60&searchTerm=treasure%20coast%20humane";
+    var apiUrlNav = "https://api.data.charitynavigator.org/v2/Organizations?app_id=72650fd4&app_key=06944884fe137960277486619c548a17&zip=20148";
+    var apiUrlEin = "https://api.data.charitynavigator.org/v2/Organizations/135563001?app_id=72650fd4&app_key=06944884fe137960277486619c548a17"
+    135563001
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+
+};
+
+getCharityData();
