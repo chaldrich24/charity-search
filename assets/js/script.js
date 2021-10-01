0a6c0ca297b7bd83e3b403d6f060a27522d2041f
Snake <chaldrich24@gmail.com>
2021-10-01 00:55:47 -0400

    js to generate list items with fetch data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,24 @@ var zipSubmit = document.getElementById("zip-submit");
 
 const API_KEY = "ac4aab77f1db8db5e50d166a738d0869";
 
-zipSubmit.addEventListener("click", function(event) {
-    // event.preventDefault();
-    var zipVal = zipInput.value;
-    if(zipVal.length <= 0) {
-        alert("very bad");
-    }
-    else {
-        getCharitiesByZip(zipVal);
-    }
-});
+zipSubmit.addEventListener("click", changePage);
 
 function getCharitiesByZip(zip) {
     var url = "https://powerful-retreat-80790.herokuapp.com/http://data.orghunter.com/v1/charitysearch?user_key=" + API_KEY + "&szipCode=" + zip;
     fetch(url)
     .then(response => response.json())
     .then(res => console.log(res))
-};
+}
+
+function changePage(event) {
+    event.preventDefault();
+    var zipVal = zipInput.value;
+    if(zipVal.length <= 0) {
+        alert("very bad")
+    }
+    else {
+        document.location.replace("./list-page.html?zip=" + zipVal);
+    }
+
+
+}
