261f7c02227521b54c7960a396cb9895561ab632
Snake <chaldrich24@gmail.com>
2021-10-05 21:12:52 -0400

    add news api

--- assets/js/charity.js
+++ assets/js/charity.js
@@ -1,4 +1,6 @@
+var newsEl = document.querySelector("#news-container");
 const API_KEY = "ac4aab77f1db8db5e50d166a738d0869";
+const NEWS_API_KEY = "a5c2b9e921dbcf681f4356e52f806b05";
 
 var getEid = function() {
     var queryString = document.location.search;
@@ -10,6 +12,18 @@ var getEid = function() {
 
 var getCharityInfo = function(ein) {
     var url = "https://powerful-retreat-80790.herokuapp.com/http://data.orghunter.com/v1/charitybasic?user_key=" + API_KEY + "&ein=" + ein;
+    fetch(url).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            displayNews(data.data.name);
+        })
+    })
+};
+
+var displayNews = function(name) {
+    var searchTerm = name.replace(/\s/g, "-");
+    console.log(searchTerm);
+    var url = "http://api.mediastack.com/v1/news?access_key=" + NEWS_API_KEY + "&languages=en&keywords=" + searchTerm;
     fetch(url).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
